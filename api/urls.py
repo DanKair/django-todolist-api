f51558712355f44d3916ca1e29af8355fa5d2b6c
DanKair <kair.daniyaruly@gmail.com>
2025-01-13 09:41:17 +0500

    Added BASE_DIR and Static root for server deployment with nginx

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,5 @@ from .views import Home
 urlpatterns = [
     path("task/", views.TaskCreate.as_view(), name="task-view-create"),
     path("task/<int:pk>", views.TaskUpdateDelete.as_view(), name="task-view-update-delete"),
-    path('', Home.as_view()),
+    path('home', Home.as_view()),
 ]
--- todoApp/settings.py
+++ todoApp/settings.py
@@ -118,7 +118,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = 'media/'
+
+# Static and Media file configuration
+import os
+from pathlib import Path
+
+# Define the BASE_DIR path
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Add the STATIC_ROOT setting
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Django Rest Framework Settings
 REST_FRAMEWORK = {
