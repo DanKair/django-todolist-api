04059326f148f955cc03605d57d1c489e09cd48f
DanKair <kair.daniyaruyl@gmail.com>
2025-01-25 15:49:43 +0500

    .gitignore created

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('', include("api.urls")),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
