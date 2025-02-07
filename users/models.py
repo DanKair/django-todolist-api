5d1e02e06a6613f59c1a8622c61728e3a802e7d2
DanKair <kair.daniyaruyl@gmail.com>
2025-02-07 17:36:17 +0500

    CustomUser model changed, Added New UserRegisterationSerializer.

--- users/models.py
+++ users/models.py
@@ -1,14 +1,51 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 # Create your models here.
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        """ Create a new user profile """
+        if not email:
+            raise ValueError('User must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        # Создание суперпользователя без указания имени пользователя
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
+
+
 class CustomUser(AbstractUser):
-    name = models.CharField(max_length = 20)
-    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=20)
+    email = models.EmailField(unique=True, blank=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    objects = CustomUserManager()
 
 
     def __str__(self):
-        return self.username
+        return self.email
+
 
--- users/serializers.py
+++ users/serializers.py
@@ -6,8 +6,22 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'email', 'password']
+        fields = ['id', 'name', 'email', 'password']
+        read_only_fields = ('email', 'username')
 
+# Before It was used to Register Users
+    """ 
     def create(self, validated_data):
         user = CustomUser.objects.create_user(**validated_data)  # Automatically hashes password
         return user
+    """
+
+class UserRegisterationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'name', 'email', 'password']
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        return CustomUser.objects.create_user(**validated_data)
+
