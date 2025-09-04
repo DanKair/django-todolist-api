fbbe2a4b8ba9e2df9e0fe931dd3b25985dd5060d
DanKair <125897962+DanKair@users.noreply.github.com>
2025-09-04 08:11:51 +0500

    Added Filtering routes

--- api/urls.py
+++ api/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path("tasks/", views.get_tasks, name="List All Tasks"),
     path("task/create", views.create_task, name="Task Create"),
     path("task/<int:id>", views.get_task_by_id, name="Access Task By ID"),
+    path("tasks/filter-by-priority/", views.filter_tasks_by_priority, name="filter-tasks-by-priority"),
+    path("tasks/priority/<int:priority_level>", views.tasks_by_priority),
+    path("tasks/filter-by-deadline/", views.filter_tasks_by_deadline, name="filter-tasks-by-deadline"),
+    path("tasks/filter-by-status/", views.filter_tasks_by_status, name="filter-tasks-by-status"),
     path('auth_check', AuthCheck.as_view()),
 ]
