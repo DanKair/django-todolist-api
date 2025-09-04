e844f9fd701c9c1712dc33c78f5a5930c0598416
DanKair <125897962+DanKair@users.noreply.github.com>
2025-09-04 08:11:23 +0500

    Added Priority Choice Field

--- api/models.py
+++ api/models.py
@@ -6,11 +6,18 @@ from users.models import CustomUser
 
 # Create your models here.
 class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ("0", "None"),
+        ("1", "High"),
+        ("2", "Medium"),
+        ("3", "Low")
+    ]
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=1)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     is_done = models.BooleanField(default=False)
+    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES, default=0)
     deadline = models.DateTimeField(blank=True, null=True)
 
-    def __str__(self):
+    def str(self):
         return self.name
